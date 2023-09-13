f560368948e647d699e8820129ce7dd6a7cb43ee
Alex Foxleigh <alex@alexfoxleigh.com>
2023-09-13 18:50:28 +0100

    Starts adding content to new site and updates layout to use the new styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import Masthead from 'components/masthead';
+import Block from 'components/block';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+    <>
+      <Masthead />
+      <main>
+        <Block name="about">
+          <h2>About Me</h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            I have been a web developer since 1998, I initially started when the
+            term was <em>&apos;web master&apos;</em> and we had to build the
+            entire site from initial design all the way up to deployment. I have
+            since specialised in front-end development and project architecture,
+            usually as the development lead.
           </p>
-        </a>
-      </div>
-    </main>
-  )
+
+          <p>
+            My primary skills centre around JavaScript, primarily the React and
+            NextJS frameworks, I have extensive experience with front-end and
+            back-end NodeJS. I&apos;m a strong advocate of Web Standards,
+            Usability, Accessibility and the DRY principle.
+          </p>
+
+          {/*TODO: <p>
+            If you are interested in knowing more about my career history, here
+            is a full account from 1998 to present.
+          </p> */}
+        </Block>
+      </main>
+    </>
+  );
 }
