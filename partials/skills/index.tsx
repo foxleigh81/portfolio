8ad6dbb55d932ecc4426e87da3de218388475ed2
Alex Foxleigh <alex@alexfoxleigh.com>
2023-09-15 14:34:54 +0100

    Uses skill grid component in partial

--- partials/skills/index.tsx
+++ partials/skills/index.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image';
 import classnames from 'classnames';
 import styles from './styles.module.scss';
 import Block from 'components/block';
+import { SkillGrid } from 'components/skill-grid';
 
 import taken from './taken.jpg';
+
+// TODO: Replace mock with api call
+import skillsMock from 'lib/mocks/skills';
 
 /* Types */
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {}
@@ -18,27 +22,27 @@ export const Skills: React.FC<Props> = ({ className, ...props }: Props) => {
   return (
     <Block name="skills" className={cx(styles['skills'], className)} {...props}>
       <h2 className={styles.h2}>
-        You&apos;ll be <em>taken</em> with my skills (sorry)
+        You&apos;ll be <em>taken</em> with my skills
       </h2>
       <div className={styles.content}>
         <Image
+          priority
           className={styles.image}
           src={taken}
           alt="Liam Neeson speaking on the phone saying 'I will find you and I will help you build a top-tier web application'"
         />
         <p>
-          I unfortunately, don’t (yet) have a robot who will bring me pizza. but
-          what I do have are a very particular set of skills; skills I have
-          acquired over a very long career. Skills that make me an asset for
-          people like you.
+          I unfortunately, don&apos;t (yet) have a robot who will bring me
+          pizza. but what I do have are a very particular set of skills; skills
+          I have acquired over a very long career. Skills that make me an asset
+          for people like you.
         </p>
         <p>
-          You can use the nifty little widget on the left to view a list of my
-          skills. The colours represent how much skill and experience I have in
-          them.
+          You can use this nifty little widget to view a list of my skills. The
+          longer the bar, the more experience I have with that particular skill.
         </p>
       </div>
-      <div>I HAZ SKILLZ</div>
+      <SkillGrid skills={skillsMock} />
     </Block>
   );
 };
