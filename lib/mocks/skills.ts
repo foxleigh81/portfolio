c54b0a112e8650aab9aab27894931360d158ec27
Alex Foxleigh <alex@alexfoxleigh.com>
2023-09-15 14:34:36 +0100

    adds a mock for skills

---
+++ lib/mocks/skills.ts
@@ -0,0 +1,139 @@
+const skills = [
+  {
+    category: 'Frontend',
+    name: 'HTML',
+    level: 10
+  },
+  {
+    category: 'Frontend',
+    name: 'CSS',
+    level: 10
+  },
+  {
+    category: 'Frontend',
+    name: 'SCSS',
+    level: 10
+  },
+  {
+    category: 'Frontend',
+    name: 'Javascript',
+    level: 10
+  },
+  {
+    category: 'Frontend',
+    name: 'React',
+    level: 10
+  },
+  {
+    category: 'Frontend',
+    name: 'Nextjs',
+    level: 9
+  },
+  {
+    category: 'Frontend',
+    name: 'Typescript',
+    level: 9
+  },
+  {
+    category: 'Frontend',
+    name: 'Node',
+    level: 9
+  },
+  {
+    category: 'Backend',
+    name: 'REST API',
+    level: 9
+  },
+  {
+    category: 'Backend',
+    name: 'GraphQL',
+    level: 7
+  },
+  {
+    category: 'Backend',
+    name: 'MongoDB',
+    level: 8
+  },
+  {
+    category: 'Backend',
+    name: 'PostgreSQL',
+    level: 7
+  },
+  {
+    category: 'Backend',
+    name: 'Firebase',
+    level: 8
+  },
+  {
+    category: 'Backend',
+    name: 'Express',
+    level: 9
+  },
+  {
+    category: 'IoT',
+    name: 'Arduino',
+    level: 7
+  },
+  {
+    category: 'IoT',
+    name: 'Raspberry Pi',
+    level: 9
+  },
+  {
+    category: 'IoT',
+    name: 'ESP32',
+    level: 8
+  },
+  {
+    category: 'IoT',
+    name: 'ESP8266',
+    level: 8
+  },
+  {
+    category: 'IoT',
+    name: 'MQTT',
+    level: 10
+  },
+  {
+    category: 'IoT',
+    name: 'Node-RED',
+    level: 10
+  },
+  {
+    category: 'IoT',
+    name: 'Home Assistant',
+    level: 10
+  },
+  {
+    category: 'Design',
+    name: 'Figma',
+    level: 9
+  },
+  {
+    category: 'Design',
+    name: 'Adobe Photoshop',
+    level: 10
+  },
+  {
+    category: 'Design',
+    name: 'Adobe Illustrator',
+    level: 10
+  },
+  {
+    category: 'Design',
+    name: 'Balsamiq',
+    level: 10
+  },
+  {
+    category: 'Design',
+    name: 'Blender',
+    level: 7
+  },
+  {
+    category: 'Design',
+    name: 'Sketchup',
+    level: 8
+  }
+];
+
+export default skills;
