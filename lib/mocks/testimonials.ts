08108998eb8aa11979aa6914150c6236df6a79d7
Alex Foxleigh <alex@alexfoxleigh.com>
2023-12-12 14:26:25 +0000

    Added testimonial data

---
+++ lib/mocks/testimonials.ts
@@ -0,0 +1,24 @@
+const testimonials = [
+  {
+    name: 'Lee Measures',
+    role: 'Senior software engineer',
+    company: 'NCSC',
+    quote: `An incredible Software Engineer with an immense background in Front End development and a mindset for architecture; his attitude towards development is positively infectious, radiating a passion for building solid foundations for scalable, damn near autonomous solutions that make for very happy customers, and teammates who have an easier job in their day to day because of his efforts. I count myself among those who have been mentored by Foxy, and I can honestly say I wouldn't be half the dev I am now without him.`
+  },
+  {
+    name: 'Mukesh Vatish',
+    role: 'Delivery Manager',
+    company: 'NCSC',
+    quote:
+      'It was a pleasure working with someone who was such an expert in his field. Alex was lead of the standards team and left a legacy that will be taken forward by the client to ensure new applications time to market are greatly reduced.'
+  },
+  {
+    name: 'Ian Mayo',
+    role: 'Director',
+    company: 'Deep Blue C',
+    quote:
+      'Alex is a great team player who worked hard to make tangible improvements to our working practices. He also invested significant time in providing us with a robust, well documented infrastructure, including the engineering improvements offered by adopting TypeScript and a multi-package repository.'
+  }
+];
+
+export default testimonials;
