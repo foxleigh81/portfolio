6ea5566bb73d0289e7f2fc88b518643b3855e8dc
Alex Foxleigh <alex@alexfoxleigh.com>
2025-01-26 18:16:08 +0000

    Updates clients mock to include logos

--- lib/mocks/clients.ts
+++ lib/mocks/clients.ts
@@ -5,18 +5,18 @@ const clients = [
     role: 'Lead Developer',
     date_started: '2024-10-01',
     date_ended: '',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/the-arch-co.jpg',
     included: true,
     description: `<p>Following the successful completion of the project at Transform, The Arch Company sought additional work and ongoing aftercare services. Recognising that these needs fell outside Transform’s operational scope, an agreement was reached for me to take on this work independently on a part-time basis.</p>
 <p>My responsibilities have included implementing new features to enhance the website’s functionality, providing expert SEO support to boost their online presence, and addressing technical issues through proactive bug fixes.</p>`
   },
   {
     slug: 'transform',
-    name: 'Transform',
+    name: 'Transform UK',
     role: 'Lead Developer',
     date_started: '2024-05-01',
     date_ended: '2024-09-01',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/transformuk.jpg',
     included: true,
     description: `<p>In my role as Lead Developer for this project, I took charge of the architecture and coding standards for a sophisticated property search platform. Managing a team of two developers, I led strategic planning discussions with clients and senior leadership to ensure alignment with business objectives.</p>
 <p>My focus on creating a robust, accessible, and performant component library, thoroughly documented and tested within Storybook, facilitated a seamless user experience and maintained high development standards.</p>`
@@ -33,22 +33,22 @@ const clients = [
   },
   {
     slug: 'surevine',
-    name: 'Surevine',
+    name: 'The Intelligence Community via Surevine',
     role: 'Head of Frontend Coding standards',
     date_started: '2021-01-01',
     date_ended: '2023-08-01',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/surevine.jpg',
     included: true,
     description: `<p>My role here was as the founder and head of the ‘Standards team’ for a major Intelligence Services commission. It was a series of projects enabling remote work for intelligence personnel by allowing day-to-day operations without traveling to a secure site.</p>
 <p>I developed a coding standard and a framework application, resulting in ‘The Wellspring’, a NextJS application that leverages Storybook and a ‘storybook-driven-development’ approach. This significantly increased development speed and quality, and I also mentored and supported other members of the commission.</p>`
   },
   {
     slug: 'cabinet-office',
-    name: 'Cabinet Office',
+    name: 'Cabinet Office Briefing Room (COBR)',
     role: 'Senior Frontend Developer',
     date_started: '2020-06-01',
     date_ended: '2020-10-01',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/cobr.jpg',
     included: true,
     description: `<p>I worked for the UK Cabinet Office's 'COBR' team, developing a NodeJS and Nunjucks-based Covid-19 response dashboard to provide a secure data-brief for the Prime Minister and cabinet. As the project lead, I built the entire front-end using Node, Express, and Nunjucks with the back-end provided via a WordPress API.</p>`
   },
@@ -58,7 +58,7 @@ const clients = [
     role: 'Head of Engineering',
     date_started: '2019-09-01',
     date_ended: '2020-06-01',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/deep-blue-c.jpg',
     included: true,
     description: `<p>My time at Deep Blue C was spent building 'Serge', a wargame engine used by various arms of the Royal Navy. It is built in React and LeafletJS. I transformed it from a simple chat client into a map-based game, creating both a pure LeafletJS version and then a React component version with react-leaflet.</p>
 <p>I also served as project architect for the Serge team, choosing and implementing the tech stack, project structure, testing strategy, and enforcing development standards.</p>`
@@ -80,7 +80,7 @@ const clients = [
     role: 'Lead Frontend Engineer',
     date_started: '2018-07-01',
     date_ended: '2019-06-01',
-    logo: 'https://placeimg.com/100/100/tech',
+    logo: '/images/logos/royal-navy.jpg',
     included: true,
     description: `<p>I worked for the Royal Navy on a project to standardize application building across various systems. This involved prototyping in VueJs and ReactJS, which led to building our 'standards toolkit' project — a suite of components (VueJS and ReactJS), CSS styles, helper functions, and guidelines (ultimately evolving into the MOD UK Design System).</p>
 <p>I also built <a href="https://docs.royalnavy.io" target="_blank">docs.royalnavy.io</a> (now <a href="https://design-system.digital.mod.uk" target="_blank">design-system.digital.mod.uk</a>) using GatsbyJS and Firebase to serve as our documentation site.</p>`
