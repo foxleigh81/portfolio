4098118b0522bf42d3c9be525c72146857f00d49
Alex Foxleigh <alex@alexfoxleigh.com>
2025-01-26 18:16:28 +0000

    Removes primary nav from nav-bar for now - will implement in phase 2

--- components/nav-bar/index.tsx
+++ components/nav-bar/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import classNames from 'classnames';
 
 /* Import Stylesheet */
 import styles from './styles.module.scss';
-import { Button } from 'components/button';
 
 import GitHub from './images/github.svg';
 import LinkedIn from './images/linkedin.svg';
+import Link from 'next/link';
 
 const cx = classNames.bind(styles);
 
@@ -19,37 +19,37 @@ const buttons = [
   },
   {
     label: 'Get in touch',
-    url: '/contact',
+    url: '#contact',
     variant: 'secondary'
   }
 ] as const;
 
-const navLinks = [
-  {
-    label: 'Home',
-    url: '/'
-  },
-  {
-    label: 'About',
-    url: '/about'
-  },
-  {
-    label: 'Skills',
-    url: '/skills'
-  },
-  {
-    label: 'Clients',
-    url: '/clients'
-  },
-  {
-    label: 'Testimonials',
-    url: '/testimonials'
-  },
-  {
-    label: 'Blog',
-    url: '/blog'
-  }
-];
+// const navLinks = [
+//   {
+//     label: 'Home',
+//     url: '#home'
+//   },
+//   {
+//     label: 'About',
+//     url: '#about'
+//   },
+//   {
+//     label: 'Skills',
+//     url: '#skills'
+//   },
+//   {
+//     label: 'Clients',
+//     url: '#clients'
+//   },
+//   {
+//     label: 'Testimonials',
+//     url: '#testimonials'
+//   },
+  // {
+  //   label: 'Blog',
+  //   url: '/blog'
+  // }
+// ];
 
 const socialLinks = [
   {
@@ -64,19 +64,17 @@ const socialLinks = [
   }
 ];
 
-const buttonAction = (url: string) => () => window.open(url);
-
 export type Props = React.HTMLAttributes<HTMLDivElement>;
 
 /**
  * The NavBar component is used to display the top navigation bar
  */
 export const NavBar: React.FC<Props> = ({ className }: Props) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
-      <div
+      {/* <div
         className={cx(
           styles['burger'],
           styles[`burger-${isMenuOpen ? 'open' : 'closed'}`]
@@ -87,15 +85,15 @@ export const NavBar: React.FC<Props> = ({ className }: Props) => {
         <div className={styles['burger-bar']} />
         <div className={styles['burger-bar']} />
         <div className={styles['burger-bar']} />
-      </div>
+      </div> */}
       <nav
         className={cx(
           styles['nav-bar'],
-          styles[`menu-${isMenuOpen ? 'open' : 'closed'}`],
+          // styles[`menu-${isMenuOpen ? 'open' : 'closed'}`],
           className
         )}
       >
-        <ul className={styles['nav-links']}>
+        {/* <ul className={styles['nav-links']}>
           {navLinks.map((link) => (
             <li key={link.label}>
               <a className={styles.link} href={link.url}>
@@ -103,7 +101,7 @@ export const NavBar: React.FC<Props> = ({ className }: Props) => {
               </a>
             </li>
           ))}
-        </ul>
+        </ul> */}
         <ul className={styles['social-links']}>
           {socialLinks.map((link) => (
             <li key={link.label}>
@@ -121,12 +119,12 @@ export const NavBar: React.FC<Props> = ({ className }: Props) => {
         <ul className={styles['button-links']}>
           {buttons.map((button) => (
             <li key={button.label}>
-              <Button
-                variant={button.variant}
-                onClick={buttonAction(button.url)}
+              <Link
+                className={styles.button}
+                href={button.url}
               >
                 {button.label}
-              </Button>
+              </Link>
             </li>
           ))}
         </ul>
