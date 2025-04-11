5960a0d128b33d14e0f764ddea30ff844c201eed
Alex Foxleigh <alex@alexfoxleigh.com>
2025-04-11 22:43:49 +0100

    Adds vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
 import 'styles/index.scss';
 import '@fontsource/work-sans';
 import type { Metadata } from 'next';
-
+import { Analytics } from '@vercel/analytics/next';
 export const metadata: Metadata = {
   title: 'Alexander Foxleigh - Lead Software Engineer',
-  description:
-    'The personal website of Alexander Foxleigh, Lead Software Engineer'
+  description: 'The personal website of Alexander Foxleigh, Lead Software Engineer',
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
